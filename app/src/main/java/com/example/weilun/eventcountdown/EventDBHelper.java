5ec65865e4dae89de4277bcbe35915c18ab4092f
Wei Lun <weilun_95@hotmail.com>
2017-08-01 20:56:15 +0800

    added EventDBHelper.java

---
+++ app/src/main/java/com/example/weilun/eventcountdown/EventDBHelper.java
@@ -0,0 +1,35 @@
+package com.example.weilun.eventcountdown;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Wei Lun on 8/1/2017.
+ */
+
+public class EventDBHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "event.db";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + EventContract.EventEntry.TABLE_NAME + " ( " +
+                    EventContract.EventEntry._ID + " INTEGER PRIMARY KEY," +
+                    EventContract.EventEntry.COLUMN_NAME_TITLE + " Text," +
+                    EventContract.EventEntry.COLUMN_NAME_DESCRIPTION + " TEXT," +
+                    EventContract.EventEntry.COLUMN_NAME_DATE + " INTEGER," +
+                    EventContract.EventEntry.COLUMN_NAME_NOTIFY + " INTEGER),";
+
+    public EventDBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
