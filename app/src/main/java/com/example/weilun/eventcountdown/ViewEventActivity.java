a2f8661687101c088b8f1c4188ec11224a4656aa
Wei Lun <weilun_95@hotmail.com>
2017-08-03 12:03:02 +0800

    added nagivationUP button

--- app/src/main/java/com/example/weilun/eventcountdown/ViewEventActivity.java
+++ app/src/main/java/com/example/weilun/eventcountdown/ViewEventActivity.java
@@ -21,6 +21,7 @@ public class ViewEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_event);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
